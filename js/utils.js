d9ea2d6c8c67a20f06fb0f59f85749da92a7ebb7
8baozhou <timchiu95@gmail.com>
2019-03-19 04:56:24 +0800

    feat

--- js/utils.js
+++ js/utils.js
@@ -1,12 +1,18 @@
 var log = console.log.bind(console)
 
-var q = function (selector, parent) {
+var q = function(selector, parent) {
     var element = parent || document
     return element.querySelector(selector)
 }
 
-var qs = function (selector, parent) {
+var qs = function(selector, parent) {
     var element = parent || document
     var list = element.querySelectorAll(selector)
     return Array.prototype.slice.call(list)
+}
+
+var bindEvent = function(element, eventName, callback) {
+    element.addEventListener(eventName, function(event) {
+        callback(event)
+    })
 }
